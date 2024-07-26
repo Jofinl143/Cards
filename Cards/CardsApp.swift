fdfdeb916f6c1c4101c0bd44f5a1647f02bd03a8
Jofin George <jofin.george@countdown.co.nz>
2024-07-27 10:07:12 +1200

    (card-1100):project setup

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,12 @@
+//  Copyright © 2024 Woolworths Group Limited. All rights reserved.
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
