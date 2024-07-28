17f577b8f567d323fe3c3a640428b9c42add42ea
Jofin George <jofin.george@countdown.co.nz>
2024-07-28 18:07:35 +1200

    (card-1100): DBHelper and Card Details

---
+++ Cards/DBManager/DBHelper.swift
@@ -0,0 +1,128 @@
+import Foundation
+import SQLite3
+
+class DBHelper {
+    init() {
+        db = openDatabase()
+        createTable()
+    }
+    
+    let dbPath: String = "myDb.sqlite"
+    var db:OpaquePointer?
+    
+    func openDatabase() -> OpaquePointer? {
+        let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            .appendingPathComponent(dbPath)
+        var db: OpaquePointer? = nil
+        if sqlite3_open(fileURL.path, &db) != SQLITE_OK {
+            print("error opening database")
+            return nil
+        } else {
+            print("Successfully opened connection to database at \(dbPath)")
+            return db
+        }
+    }
+    
+    func createTable() {
+        let createTableString = "CREATE TABLE IF NOT EXISTS CARD(Id INTEGER PRIMARY KEY,uid TEXT,creditCardNumber TEXT,creditCardExpiryDate TEXT,creditCardType TEXT, isCardSaved INTEGER);"
+        var createTableStatement: OpaquePointer? = nil
+        if sqlite3_prepare_v2(db, createTableString, -1, &createTableStatement, nil) == SQLITE_OK {
+            if sqlite3_step(createTableStatement) == SQLITE_DONE {
+                print("card table created.")
+            } else {
+                print("card table could not be created.")
+            }
+        } else {
+            print("CREATE TABLE statement could not be prepared.")
+        }
+        sqlite3_finalize(createTableStatement)
+    }
+    
+    func insert(id: Int, uid: String, credit_card_number: String, credit_card_expiry_date: String, credit_card_type: String, isCardSaved: Int) {
+        let cards = read()
+        for c in cards {
+            if c.id == id {
+                return
+            }
+        }
+        let insertStatementString = "INSERT INTO CARD (Id, uid, creditCardNumber, creditCardExpiryDate, creditCardType, isCardSaved) VALUES (?, ?, ?, ?, ?, ?);"
+        var insertStatement: OpaquePointer? = nil
+        if sqlite3_prepare_v2(db, insertStatementString, -1, &insertStatement, nil) == SQLITE_OK {
+            sqlite3_bind_int(insertStatement, 1, Int32(id))
+            sqlite3_bind_text(insertStatement, 2, (uid as NSString).utf8String, -1, nil)
+            sqlite3_bind_text(insertStatement, 3, (credit_card_number as NSString).utf8String, -1, nil)
+            sqlite3_bind_text(insertStatement, 4, (credit_card_expiry_date as NSString).utf8String, -1, nil)
+            sqlite3_bind_text(insertStatement, 5, (credit_card_type as NSString).utf8String, -1, nil)
+            sqlite3_bind_int(insertStatement, 6, Int32(isCardSaved))
+            
+            if sqlite3_step(insertStatement) == SQLITE_DONE {
+                print("Successfully inserted row.")
+            } else {
+                print("Could not insert row.")
+            }
+        } else {
+            print("INSERT statement could not be prepared.")
+        }
+        sqlite3_finalize(insertStatement)
+    }
+    
+    func read() -> [Card] {
+        let queryStatementString = "SELECT * FROM CARD;"
+        var queryStatement: OpaquePointer? = nil
+        var cards : [Card] = []
+        if sqlite3_prepare_v2(db, queryStatementString, -1, &queryStatement, nil) == SQLITE_OK {
+            while sqlite3_step(queryStatement) == SQLITE_ROW {
+                let id = sqlite3_column_int(queryStatement, 0)
+                let uid = String(describing: String(cString: sqlite3_column_text(queryStatement, 1)))
+                let credit_card_number = String(describing: String(cString: sqlite3_column_text(queryStatement, 2)))
+                let credit_card_expiry_date = String(describing: String(cString: sqlite3_column_text(queryStatement, 3)))
+                let credit_card_type = String(describing: String(cString: sqlite3_column_text(queryStatement, 4)))
+                let isCardSaved = sqlite3_column_int(queryStatement, 5)
+                cards.append(Card(id: Int(id), uid: uid, credit_card_number: credit_card_number, credit_card_expiry_date: credit_card_expiry_date, credit_card_type: credit_card_type, isCardSaved: isCardSaved))
+                print("Query Result:")
+                print("\(id) | \(uid) | \(credit_card_number) | \(credit_card_expiry_date)| \(credit_card_type)")
+            }
+        } else {
+            print("SELECT statement could not be prepared")
+        }
+        sqlite3_finalize(queryStatement)
+        return cards
+    }
+    
+    func deleteCardByID(id: Int) {
+        let deleteStatementStirng = "DELETE FROM CARD WHERE Id = ?;"
+        var deleteStatement: OpaquePointer? = nil
+        if sqlite3_prepare_v2(db, deleteStatementStirng, -1, &deleteStatement, nil) == SQLITE_OK {
+            sqlite3_bind_int(deleteStatement, 1, Int32(id))
+            if sqlite3_step(deleteStatement) == SQLITE_DONE {
+                print("Successfully deleted row.")
+            } else {
+                print("Could not delete row.")
+            }
+        } else {
+            print("DELETE statement could not be prepared")
+        }
+        sqlite3_finalize(deleteStatement)
+    }
+    
+    func isCardAvailable(currentId: Int) -> Bool {
+        let queryStatementString = "SELECT * FROM card Where Id = ?;"
+        var queryStatement: OpaquePointer? = nil
+        if sqlite3_prepare_v2(db, queryStatementString, -1, &queryStatement, nil) == SQLITE_OK {
+            if sqlite3_step(queryStatement) == SQLITE_ROW {
+                let id = sqlite3_column_int(queryStatement, 0)
+                guard let queryResultCol1 = sqlite3_column_text(queryStatement, 1) else {
+                    print("Query result is nil")
+                    return false
+                }
+                print("\nQuery Result:")
+                return currentId == id
+            } else {
+                print("\nQuery returned no results.")
+            }
+        } else {
+            print("statement could not be prepared")
+        }
+        return false
+    }
+}
---
+++ Cards/Modules/CardDetailsView.swift
@@ -0,0 +1,50 @@
+import SwiftUI
+
+struct CardDetailsView: View {
+    let card: Card
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            
+            HStack {
+                Text("Nicolas Martin")
+                    .font(.system(size: 18, weight: .medium, design: .serif))
+            }
+            .padding()
+            
+            Text(card.credit_card_number)
+                    .font(.system(size: 25, weight: .semibold, design: .rounded))
+            .padding()
+            
+            Image(systemName: "esim")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 30)
+                .padding()
+            
+            HStack {
+                Text(card.credit_card_type)
+                    .font(.system(size: 20, weight: .semibold, design: .monospaced))
+                Spacer()
+                Text(card.credit_card_expiry_date)
+                    .font(.system(size: 14, weight: .medium))
+            }
+            .padding()
+        }
+        .foregroundColor(.white)
+        .background(
+            LinearGradient(colors: [Color(red: 176/255, green: 143/255, blue: 38/255, opacity: 1), Color.black], startPoint: .top, endPoint: .bottom)
+        )
+        .overlay(RoundedRectangle(cornerRadius: 6)
+            .stroke(Color.black.opacity(0.5), lineWidth: 1)
+        )
+        .cornerRadius(8)
+        .shadow(radius: 4)
+        .padding(.horizontal)
+        .padding(.top, 8)
+    }
+}
+
+#Preview {
+    CardDetailsView(card: Card(id: 1, uid: "123", credit_card_number: "123457894", credit_card_expiry_date: "11 Sept", credit_card_type: "Visa", isCardSaved: 1))
+}
